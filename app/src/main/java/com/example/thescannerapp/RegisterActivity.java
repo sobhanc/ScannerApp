460173829a58038c7accc25c8ac60ebb1113cdb8
grimc <sobhanchohan@gmail.com>
2020-10-28 15:24:56 -0400

    First Commit for ScannerApp

---
+++ app/src/main/java/com/example/thescannerapp/RegisterActivity.java
@@ -0,0 +1,65 @@
+package com.example.thescannerapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+    DatabaseHelper db;
+    EditText mTextUsername;
+    EditText mTextPassword;
+    EditText mTextCnfPassword;
+    Button mButtonRegister;
+    TextView mTextViewLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        db = new DatabaseHelper(this);
+        mTextUsername = (EditText)findViewById(R.id.edittext_username);
+        mTextPassword = (EditText)findViewById(R.id.edittext_password);
+        mTextCnfPassword = (EditText)findViewById(R.id.edittext_cnf_password);
+        mButtonRegister = (Button)findViewById(R.id.button_register);
+        mTextViewLogin = (TextView)findViewById(R.id.textview_login);
+        mTextViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent LoginIntent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(LoginIntent);
+            }
+        });
+
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = mTextUsername.getText().toString().trim();
+                String pwd = mTextPassword.getText().toString().trim();
+                String cnf_pwd = mTextCnfPassword.getText().toString().trim();
+
+                //user validation
+                if(user.isEmpty()) {
+                    mTextUsername.setError("Please enter a username!");
+                } else if(pwd.isEmpty()) {
+                    mTextPassword.setError("Please enter a password!");
+                } else if(cnf_pwd.isEmpty()) {
+                    mTextCnfPassword.setError("Please confirm password!");
+                } else if (pwd.equals(cnf_pwd))  {
+                    long val = db.addUser(user,pwd);
+                    Toast.makeText(RegisterActivity.this,"You have registered an account",Toast.LENGTH_SHORT).show();
+                    Intent moveToLogin = new Intent(RegisterActivity.this,LoginActivity.class);
+                    startActivity(moveToLogin);
+                } else {
+                    Toast.makeText(RegisterActivity.this,"Password does not match",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+}
