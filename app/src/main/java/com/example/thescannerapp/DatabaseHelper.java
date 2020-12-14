eaada6e2fa5f458a96554c74be4b8efacc0dfee8
Dylan Dominguez <domi7912@hotmail.com>
2020-12-13 19:53:43 -0500

    Updated the comments, fixed some bugs, and added the DatabaseContract class. The functionality is the same, just now following google recommendations

--- app/src/main/java/com/example/thescannerapp/DatabaseHelper.java
+++ app/src/main/java/com/example/thescannerapp/DatabaseHelper.java
@@ -4,49 +4,64 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import static com.example.thescannerapp.DatabaseContract.*;
 
 public class DatabaseHelper extends SQLiteOpenHelper{
-    public static final String DATABASE_NAME ="register.db";
-    public static final String TABLE_NAME ="registeruser";
-    public static final String COL_1 ="ID";
-    public static final String COL_2 ="username";
-    public static final String COL_3 ="password";
 
+    // Database creation sql statement
+    //Parent Table
+    private static final String SQL_CREATE_REGISTRY =
+            "CREATE TABLE " + RegistrationDB.TABLE_NAME + " (" +
+                    RegistrationDB._ID + " INTEGER PRIMARY KEY," +
+                    RegistrationDB.COLUMN_USERNAME+ " TEXT," +
+                    RegistrationDB.COLUMN_PASSWORD + " TEXT)";
+    private static final String DATABASE_NAME = "register.db";
+    private static final int DATABASE_VERSION = 1;
+
+    //constructor
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, 1);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("CREATE TABLE registeruser (ID INTEGER PRIMARY  KEY AUTOINCREMENT, username TEXT, password TEXT)");
+        //create the database
+        sqLiteDatabase.execSQL(SQL_CREATE_REGISTRY);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL(" DROP TABLE IF EXISTS " + TABLE_NAME);
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        Log.w(DatabaseHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        //delete old data
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + RegistrationDB.TABLE_NAME);
+
+        //recreate tables
         onCreate(sqLiteDatabase);
     }
-
+    //this will add the new user to the database
     public long addUser(String user, String password){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put("username",user);
-        contentValues.put("password",password);
-        long res = db.insert("registeruser",null,contentValues);
+        contentValues.put(RegistrationDB.COLUMN_USERNAME,user);
+        contentValues.put(RegistrationDB.COLUMN_PASSWORD,password);
+        long res = db.insert(RegistrationDB.TABLE_NAME,null,contentValues);
         db.close();
         return  res;
     }
-
+    //this is used as a validation for the login
     public boolean checkUser(String username, String password){
-        String[] columns = { COL_1 };
+        String[] columns = { RegistrationDB._ID };
         SQLiteDatabase db = getReadableDatabase();
-        String selection = COL_2 + "=?" + " and " + COL_3 + "=?";
+        String selection = RegistrationDB.COLUMN_USERNAME + "=?" + " and " + RegistrationDB.COLUMN_PASSWORD + "=?";
         String[] selectionArgs = { username, password };
-        Cursor cursor = db.query(TABLE_NAME,columns,selection,selectionArgs,null,null,null);
+        Cursor cursor = db.query(RegistrationDB.TABLE_NAME,columns,selection,selectionArgs,null,null,null);
         int count = cursor.getCount();
         cursor.close();
         db.close();
-
         return count > 0;
     }
 }
--- app/src/main/java/com/example/thescannerapp/LoginActivity.java
+++ app/src/main/java/com/example/thescannerapp/LoginActivity.java
@@ -24,26 +24,31 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
+        //Binds of all the views
         db = new DatabaseHelper(this);
         mTextUsername = findViewById(R.id.edittext_username);
         mTextPassword = findViewById(R.id.edittext_password);
         mButtonLogin = findViewById(R.id.button_login);
         mTextViewRegister = findViewById(R.id.textview_register);
+        //The register button launches new activity using registerIntent
         mTextViewRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent registerIntent = new Intent(LoginActivity.this,RegisterActivity.class);
                 startActivity(registerIntent);
+                finish();
             }
         });
-
+        //login will launch the new homeActivity
         mButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String user = mTextUsername.getText().toString().trim();
                 String pwd = mTextPassword.getText().toString().trim();
+                //db is an instance of DatabaseHelper checkUser will check against the database the username and password
+                //will return true/false according to the input
                 Boolean res = db.checkUser(user, pwd);
+                //this is a short validation if statement
                 if(res == true)
                 {
                     Intent HomePage = new Intent(LoginActivity.this, HomeActivity.class);
